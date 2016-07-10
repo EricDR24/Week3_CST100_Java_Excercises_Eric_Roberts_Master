c7d54f692c408c971209ffc9f8d7c391577a5027
Eric Roberts <eric.dan.roberts@gmail.com>
2016-07-10 13:05:01 -0700

    new folder

---
+++ stringPermutation.java
@@ -0,0 +1,41 @@
+/** Program:  18.25 String permutation
+  * File:     stringPermutation.java 
+  * Summary:  displays all the permutations of a string
+  * Author:   Eric Roberts
+  * Date:     July 3, 2016
+**/
+import java.util.Scanner;
+
+public class stringPermutation {
+
+	public static void main(String[] args) {
+		//Start Scanner for String
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String s = sc.nextLine();
+		
+		//display permutations
+		System.out.println("Permutations of " + s + " are: ");
+		
+		//method
+		displayPermutation(s);
+
+	}
+
+	public static void displayPermutation(String s) {
+		displayPermutation("", s);
+		
+	}
+
+	public static void displayPermutation(String s1, String s2) {
+		if (s2.length() == 0) {
+			System.out.println(s1);
+		} else {
+			for (int i = 0; i < s2.length(); i++) {
+				displayPermutation(s1 + s2.charAt(i), s2.substring(0, i) + s2.substring(i + 1));
+			}
+		}
+		
+	}
+
+}
